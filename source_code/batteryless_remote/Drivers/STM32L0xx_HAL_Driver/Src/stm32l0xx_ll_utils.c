75e02c918548a706ad7aa93e679eac54ef70f1cb
bbear2 <bbear_mail@163.com>
2022-07-07 01:19:13 +0800

    change the LLmDelay to LLuDelay

--- source_code/batteryless_remote/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_ll_utils.c
+++ source_code/batteryless_remote/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_ll_utils.c
@@ -147,7 +147,7 @@ static ErrorStatus UTILS_PLL_IsBusy(void);
 void LL_Init1msTick(uint32_t HCLKFrequency)
 {
   /* Use frequency provided in argument */
-  LL_InitTick(HCLKFrequency, 1000U);
+  LL_InitTick(HCLKFrequency, 1000000U);
 }
 
 /**
