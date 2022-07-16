0864113ea7c0344a342bfae08e38b3e500cba36f
bbear2 <bbear_mail@163.com>
2022-07-16 17:52:42 +0800

    change flash read/write buf type from uin32_t to uint8_t

--- source_code/batteryless_remote/Core/Inc/eeprom.h
+++ source_code/batteryless_remote/Core/Inc/eeprom.h
@@ -17,14 +17,14 @@ extern "C" {
 #include "stdint.h"
 #include "stm32g030xx.h"
 
-#define EEPROM_START_ADDR 0x08007000
-#define EEPROM_END_ADDR   0x08007fff
+#define IRDATA_START_ADDR 0x08007000
+#define IRDATA_END_ADDR   0x08007fff
 
 #define FLASH_PEKEY1 0x45670123
 #define FLASH_PEKEY2 0xcdef89ab
 
-int16_t eeprom_read(uint32_t *addr, uint32_t *buf, uint16_t size);
-int16_t eeprom_write(uint32_t *addr, uint32_t *buf, uint16_t size, uint32_t timeout);
+int16_t eeprom_read(uint32_t *addr, uint8_t *buf, uint16_t size);
+int16_t eeprom_write(uint32_t *addr, uint8_t *buf, uint16_t size, uint32_t timeout);
 
 #ifdef __cplusplus
 }
--- source_code/batteryless_remote/Core/Src/eeprom.c
+++ source_code/batteryless_remote/Core/Src/eeprom.c
@@ -4,39 +4,57 @@
  * @LastEditors: bbear2 bbear_mail@163.com
  * @LastEditTime: 2022-07-13 01:02:07
  * @FilePath: \batteryless_remote\Core\Src\eeprom.c
- * @Description: internal eeprom read/write implementation
+ * @Description: internal flash read/write implementation
  */
 #include "eeprom.h"
 #include "log.h"
 
 /**
- * @description: eeprom read
+ * @description: flash read
  * @param {uin32_t} *addr
  * @param {uint8_t} *buf
  * @param {uint16_t} size
- * @param {uint32_t} timeout unit:ms
  * @return {*}
  */
-int16_t eeprom_read(uint32_t *addr, uint32_t *buf, uint16_t size)
+int16_t eeprom_read(uint32_t *addr, uint8_t *buf, uint16_t size)
 {
     uint32_t result = IR_OK;
-    uint16_t i = 0;
+    uint16_t i = 0, j = 0;
     uint32_t temp = addr;
+    uint32_t read_data = 0x00;
 
-    if ((temp % 4) || addr > EEPROM_END_ADDR || \
-        addr < EEPROM_START_ADDR || !buf || !size)
+    if ((temp % 8) || addr > IRDATA_END_ADDR || \
+        addr < IRDATA_START_ADDR || !buf || !size)
         return -IR_INVAL;
 
     //read data from eeprom
-    while(i < size)
+    while(i < size / 4)
     {
-        if(addr + i > EEPROM_END_ADDR)
-            break;
-        buf[i] = *(addr + i);
-        i++;
+        if(addr + i * 4 > IRDATA_END_ADDR)
+        {
+            Log_Printf("read addr overflow\n");
+            result = i * 4;
+            goto exit;
+        }
+        read_data = *(addr + i);
+        buf[j + 0] = read_data >> 24 & 0xff;
+        buf[j + 1] = read_data >> 16 & 0xff;
+        buf[j + 2] = read_data >> 8 & 0xff;
+        buf[j + 3] = read_data >> 0 & 0xff;
+        j += 4;
+        i += 1;
     }
-    result = i;
+    if(size % 4)
+    {
+        read_data = *(addr + (size / 4));
+        for(i = 0;i < size - j; ++i)
+        {
+            buf[j + i] = (read_data >> ((4 - 1 - i) * 8)) & 0xff;
+        }
+    }
+    result = size;
 
+exit:
     return result;
 }
 
@@ -48,15 +66,19 @@ int16_t eeprom_read(uint32_t *addr, uint32_t *buf, uint16_t size)
  * @param {uint32_t} timeout unit:ms
  * @return {*}
  */
-int16_t eeprom_write(uint32_t *addr, uint32_t *buf, uint16_t size, uint32_t timeout)
+int16_t eeprom_write(uint32_t *addr, uint8_t *buf, uint16_t size, uint32_t timeout)
 {
     uint32_t time_cnt = 0;//ms
     uint32_t result = IR_OK;
-    uint16_t i = 0;
+    uint16_t i = 0,j = 0;
     uint32_t temp = addr;
+    uint32_t write_data1 = 0x00;
+    uint32_t write_data2 = 0x00;
+    uint16_t size_of_uint32 = 0;
+    uint32_t temp_uint32_buf[2] = { 0 };
 
-    if ((temp % 4) || addr > EEPROM_END_ADDR || \
-        addr < EEPROM_START_ADDR || !buf || !size)
+    if ((temp % 8) || addr > IRDATA_END_ADDR || \
+        addr < IRDATA_START_ADDR || !buf || !size)
         return -IR_INVAL;
 
     //Unlock the internal flash
@@ -86,16 +108,42 @@ int16_t eeprom_write(uint32_t *addr, uint32_t *buf, uint16_t size, uint32_t time
 
     //Write data to eeprom
     FLASH->CR |= 0x00000001;
-    while(i < size)
+    size_of_uint32 = size / 4;
+    while(i < size_of_uint32)
     {
-        if(addr + i > EEPROM_END_ADDR)
-            break;
-        *(addr + i) = buf[i];
-        *(addr + i + 1) = buf[i + 1];
+        if(addr + i * 4 > IRDATA_END_ADDR)
+        {
+            Log_Printf("write addr overflow\n");
+            result = i * 4;
+            goto exit;
+        }
+        *(addr + i) = buf[j] << 24 | buf[j+1] << 16 | buf[j+2] << 8 | buf[j+3];
+        *(addr + i + 1) = buf[j+4] << 24 | buf[j+5] << 16 | buf[j+6] << 8 | buf[j+7];
         i += 2;
+        j += 8;
     }
-    result = i;
+    if(size % 8 != 0)
+    {
+        if(addr + i * 4 > IRDATA_END_ADDR)
+        {
+            Log_Printf("write addr overflow\n");
+            result = i * 4;
+            goto exit;
+        }
+        for(i = 0;i < size - (size / 8) * 8; ++i)
+        {
+            if(i < 4)
+                temp_uint32_buf[0] |= buf[(size / 8) * 8 + i] << ((4 - 1 - i) * 8);
+            else
+                temp_uint32_buf[1] |= buf[(size / 8) * 8 + i] << ((8 - 1 - i) * 8);
+        }
 
+        *(addr + (size / 8) * 2) = temp_uint32_buf[0];
+        *(addr + (size / 8) * 2 + 1) = temp_uint32_buf[1];
+    }
+    result = size;
+
+exit:
     //Wait for flash write finish
     while ((FLASH->SR & 0x00010000) != 0)
     {
@@ -125,6 +173,5 @@ int16_t eeprom_write(uint32_t *addr, uint32_t *buf, uint16_t size, uint32_t time
         return -IR_ERROR;
     }
 
-exit:
     return result;
 }
