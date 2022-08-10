5816dd093234db5c911ae2ec546fac8aafee7014
bbear2 <bbear_mail@163.com>
2022-08-11 01:17:56 +0800

    delete the code of uart rx because we will use it as a button

--- source_code/batteryless_remote/Core/Src/usart.c
+++ source_code/batteryless_remote/Core/Src/usart.c
@@ -42,16 +42,8 @@ void MX_USART1_UART_Init(void)
 
   LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOB);
   /**USART1 GPIO Configuration
-  PB7   ------> USART1_RX
   PB6   ------> USART1_TX
   */
-  GPIO_InitStruct.Pin = LL_GPIO_PIN_7;
-  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-  GPIO_InitStruct.Alternate = LL_GPIO_AF_0;
-  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   GPIO_InitStruct.Pin = LL_GPIO_PIN_6;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
